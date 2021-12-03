90012fb873355f1b2449be9770cce35ee1d3fb50
Cristian Urbainski <cristianurbainskips@gmail.com>
2021-12-03 18:31:21 -0300

    Ajustes:
    - Adicionado bootstrap ao projeto;
    - Adicionado componentes de fotos
    - Ajustado tela principal do app para mostrar uma lista de fotos utilizando o componente criado;

--- angular/alurapic/src/app/app.component.ts
+++ angular/alurapic/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'alurapic';
+	
+    title = "alurapic";
+
+    photos = [
+        {
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+          description: 'Leão'
+        },
+        {
+          url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+          description: 'Leoa'
+        },
+    ];
+
 }
