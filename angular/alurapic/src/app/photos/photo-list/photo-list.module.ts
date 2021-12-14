9267b3d5c51f48d926531fc416244689487ec863
Cristian Urbainski <cristianurbainskips@gmail.com>
2021-12-14 19:02:00 -0300

    - Adicionado componente card - Usado componente de card para mostrar as fotos - Ajustado path do arquivo css dos icones

--- angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/components/card/card.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
 	declarations: [
@@ -16,6 +19,7 @@ import { PhotoModule } from '../photo/photo.module';
 	imports: [
 		CommonModule,
 		PhotoModule,
+		CardModule,
 	]
 })
 export class PhotoListModule { }
