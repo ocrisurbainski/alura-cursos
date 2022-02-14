34b3bc268aaffbc8db1b064c5e00fca3487cdb0c
Cristian Urbainski <cristianurbainskips@gmail.com>
2022-02-14 09:37:32 -0300

    Criado menu para o sistema

--- angular/alurapic/src/app/core/core.module.ts
+++ angular/alurapic/src/app/core/core.module.ts
@@ -8,6 +8,8 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
     declarations: [
@@ -23,6 +25,8 @@ import { LoadingModule } from '../shared/components/loading/loading.module';
         RouterModule,
         AlertModule,
         LoadingModule,
+        MenuModule,
+        ShowIfLoggedModule,
     ],
     providers: [
         {
