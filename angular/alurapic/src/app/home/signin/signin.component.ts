d0a4fb94ee691b8cb7c872639115f31a356c0fc7
Cristian Urbainski <cristianurbainskips@gmail.com>
2022-02-15 23:37:35 -0300

    Implementado queryParams para redirecionamento do login para um tela especifica do sistema

--- angular/alurapic/src/app/home/signin/signin.component.ts
+++ angular/alurapic/src/app/home/signin/signin.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { PlatformDetectorService } from 'src/app/core/platform/platform-detector.service';
 
@@ -8,7 +8,9 @@ import { PlatformDetectorService } from 'src/app/core/platform/platform-detector
     selector: 'app-signin',
     templateUrl: './signin.component.html',
 })
-export class SigninComponent implements AfterViewInit {
+export class SigninComponent implements OnInit, AfterViewInit {
+
+    fromUrl: string = '';
 
     loginForm: FormGroup;
 
@@ -18,12 +20,17 @@ export class SigninComponent implements AfterViewInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
+        private activatedRoute: ActivatedRoute,
         private platformDetectorService: PlatformDetectorService) { 
 
         this.loginForm = this.formBuilder.group({
             username: ['', [Validators.required, Validators.minLength(3)]],
             password: ['', [Validators.required, Validators.minLength(3)]]
         });
+    }
+
+    ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(params => this.fromUrl = params['fromUrl']);
     }
 
     ngAfterViewInit(): void {
@@ -37,7 +44,13 @@ export class SigninComponent implements AfterViewInit {
             password = data.password;
 
         this.authService.authenticate(username, password).subscribe({
-            next: () => this.router.navigate(['user', username]),
+            next: () => {
+                if (this.fromUrl.length > 0) {
+                    this.router.navigateByUrl(this.fromUrl);
+                } else {
+                    this.router.navigate(['user', username])
+                }
+            },
             error: (err) => {
                 this.loginForm.reset();
                 this.platformDetectorService.isPlatformBrowser() && this.usernameInput.nativeElement.focus();
--- angular/alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ angular/alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -38,7 +38,7 @@ export class PhotoDetailsComponent implements OnInit {
         this.photoService.removePhoto(this.photoId).subscribe({
             next: () => {
                 this.alertService.success('Photo removed', true);
-                this.router.navigate(['/user', this.userService.getUserName()]);
+                this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
             },
             error: (err) => {
                 console.log(err);
