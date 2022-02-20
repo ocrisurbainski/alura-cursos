a5f101d38a20b17fcabafe1b63813ad59cd1d195
Cristian Urbainski <cristianurbainskips@gmail.com>
2022-02-20 17:21:57 -0300

    Adicionado parametro ao decorator para definir se o tempo de execução deve ser mostrado em segundos ou millisegundos, sendo que por padrão é mostrado em millisegundos.

--- typescript/typescript-curso/app/dist/js/decorators/metrica-tempo-execucao.js
+++ typescript/typescript-curso/app/dist/js/decorators/metrica-tempo-execucao.js
@@ -1,13 +1,20 @@
-export function metricaTempoExcecucao() {
+export function metricaTempoExcecucao(showMetricasEmSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
             const t1 = performance.now();
             const retornoMetodoOriginal = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`Método \'${propertyKey}\' tempo de execução: ${(t2 - t1) / 1000}`);
+            console.log(`Método \'${propertyKey}\' tempo de execução: ${calcular(t1, t2, showMetricasEmSegundos)}`);
             return retornoMetodoOriginal;
         };
         return descriptor;
     };
+}
+function calcular(t1, t2, showMetricasEmSegundos) {
+    const diff = t2 - t1;
+    if (showMetricasEmSegundos) {
+        return `${diff / 1000} segundos`;
+    }
+    return `${diff} millisegundos`;
 }
--- typescript/typescript-curso/app/src/decorators/metrica-tempo-execucao.ts
+++ typescript/typescript-curso/app/src/decorators/metrica-tempo-execucao.ts
@@ -1,4 +1,4 @@
-export function metricaTempoExcecucao() {
+export function metricaTempoExcecucao(showMetricasEmSegundos: boolean = false) {
     return function(
         target: any,
         propertyKey: any,
@@ -13,11 +13,20 @@ export function metricaTempoExcecucao() {
 
             const t2 = performance.now();
 
-            console.log(`Método \'${propertyKey}\' tempo de execução: ${(t2 - t1) / 1000}`);
+            console.log(`Método \'${propertyKey}\' tempo de execução: ${calcular(t1, t2, showMetricasEmSegundos)}`);
 
             return retornoMetodoOriginal;
         };
 
         return descriptor;
     }
+}
+
+function calcular(t1: number, t2: number, showMetricasEmSegundos: boolean): string {
+
+    const diff = t2 - t1;
+    if (showMetricasEmSegundos) {
+        return `${diff / 1000} segundos`;
+    }
+    return `${diff} millisegundos`;
 }
