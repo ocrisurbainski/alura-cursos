a0fd4d8799d95c63f81908bb6c7a1dc6716d4fa7
cristian.urbainski <Cristian Urbainski>
2022-05-11 18:14:32 -0300

    Ajustado para enviar 10 pedidos por vez para os topicos do kafka.

--- kafka/ecommerce/src/main/java/br/com/urbainski/ecommerce/order/NewOrderMain.java
+++ kafka/ecommerce/src/main/java/br/com/urbainski/ecommerce/order/NewOrderMain.java
@@ -7,18 +7,17 @@ import java.util.UUID;
 public class NewOrderMain {
 
     public static void main(String[] args) {
-
-        var userId = UUID.randomUUID();
-
-        var value = String.format("%s,1,1250.00", userId.toString());
-
         try (var newOrderService = new NewOrderService()) {
-            newOrderService.send(userId.toString(), value);
+            try (var emailService = new EmailProducerService()) {
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID();
+                    var value = String.format("%s,1,1250.00", userId.toString());
+                    newOrderService.send(userId.toString(), value);
+                    emailService.send(userId.toString(), "Sua venda está sendo processada, aguarde...");
+                }
+            }
         }
 
-        try (var emailService = new EmailProducerService()) {
-            emailService.send(userId.toString(), "Sua venda está sendo processada, aguarde...");
-        }
     }
 
 
