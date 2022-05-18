405dce98324d6e749ec9f98bb630663fe82b9eef
cristian.urbainski <Cristian Urbainski>
2022-05-18 13:27:42 -0300

    Confirmação de recebimento das mensagens.

--- kafka/ecommerce/commons/src/main/java/br/com/urbainski/ecommerce/commons/kafka/KafkaProperties.java
+++ kafka/ecommerce/commons/src/main/java/br/com/urbainski/ecommerce/commons/kafka/KafkaProperties.java
@@ -15,31 +15,23 @@ public final class KafkaProperties {
     }
 
     public static Properties getKafkaProducerProperties(KafkaTypes keyType, KafkaTypes valueType) {
-        var properties = getDefaultKafkaProducerProperties();
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        // confirmação de todas as replicas do kafka receberão a mensagem
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keyType.getClassSerializer());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueType.getClassSerializer());
         return properties;
     }
 
-    private static Properties getDefaultKafkaProducerProperties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-        return properties;
-    }
-
     public static Properties getKafkaConsumerProperties(String groudId, KafkaTypes keyType, KafkaTypes valueType, String typeClass) {
-        var properties = getDefaultKafkaConsumerProperties(groudId);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyType.getClassDeserializer());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueType.getClassDeserializer());
-        properties.setProperty(TYPE_CLASS_CONFIG, typeClass);
-        return properties;
-    }
-
-    private static Properties getDefaultKafkaConsumerProperties(String groudId) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groudId);
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyType.getClassDeserializer());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueType.getClassDeserializer());
+        properties.setProperty(TYPE_CLASS_CONFIG, typeClass);
         return properties;
     }
 
