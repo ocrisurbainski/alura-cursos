f69645b3b68a8a1ab2da3390f4126bd0d0e47c8a
cristian.urbainski <Cristian Urbainski>
2022-06-15 14:55:00 -0300

    Adicionado paginação e ordenação ao findAll

--- java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -33,9 +35,7 @@ public class TopicosController {
 	@GetMapping
 	public Page<TopicoDto> lista(
 			@RequestParam(required = false) String nomeCurso,
-			@RequestParam Integer page,
-			@RequestParam Integer size) {
-		Pageable pageable = PageRequest.of(page, size);
+			@PageableDefault Pageable pageable) {
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(pageable);
 			return TopicoDto.converter(topicos);
