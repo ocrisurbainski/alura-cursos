88fe92a5817104f610760bffa79f28caa01e3f1d
cristian.urbainski <Cristian Urbainski>
2022-06-15 15:42:20 -0300

    Habilitado o cache no sistema.

--- java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ java/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,10 +8,9 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +32,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(
 			@RequestParam(required = false) String nomeCurso,
 			@PageableDefault Pageable pageable) {
