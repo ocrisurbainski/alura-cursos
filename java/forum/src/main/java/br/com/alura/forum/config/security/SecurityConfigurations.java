c857be8b8b38bfdb55e1e4d927fafb3dba7ba00f
cristian.urbainski <Cristian Urbainski>
2022-06-16 07:29:04 -0300

    Adicionado o spring boot actuator ao projeto.

--- java/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ java/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -48,7 +48,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
