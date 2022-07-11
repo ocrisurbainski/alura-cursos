718bfd6b15bb5641615ce5b9230c70f8fbea9a71
cristian.urbainski <Cristian Urbainski>
2022-07-11 15:55:08 -0300

    Criado teste de integração para o repository do usuario.

---
+++ java/forum/src/test/java/br/com/alura/forum/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void deveriaCarregarUmUsuarioAoBuscarPeloSeuEmail() {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail("aluno@email.com");
+        Assertions.assertTrue(usuarioOptional.isPresent());
+        Assertions.assertEquals("aluno@email.com", usuarioOptional.get().getEmail());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmUsuarioQueNaoEstaCadastrado() {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail("qualquer@email.com");
+        Assertions.assertFalse(usuarioOptional.isPresent());
+    }
+
+}
