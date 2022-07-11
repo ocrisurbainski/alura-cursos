ca1bd48b4d616585c643376726a7e3e77a6306f5
cristian.urbainski <Cristian Urbainski>
2022-07-11 15:48:40 -0300

    Criado teste de integração para o repository do curso.

---
+++ java/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Curso;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        Curso curso = cursoRepository.findByNome("HTML 5");
+        Assertions.assertNotNull(curso);
+        Assertions.assertEquals("HTML 5", curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoQueNaoEstaCadastrado() {
+        Curso curso = cursoRepository.findByNome("Java + JPA");
+        Assertions.assertNull(curso);
+    }
+
+}
