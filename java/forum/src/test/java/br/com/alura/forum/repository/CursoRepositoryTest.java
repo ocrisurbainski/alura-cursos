48308cb4f7210bcb79b5e997df6af8c81426a67f
cristian.urbainski <Cristian Urbainski>
2022-07-11 16:05:31 -0300

    Criado novo profile para teste e aplicado ajustes para o profile de teste, configurado outro nome de banco de dados.

--- java/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ java/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRepositoryTest {
 
     @Autowired
--- java/forum/src/test/java/br/com/alura/forum/repository/TopicoRepositoryTest.java
+++ java/forum/src/test/java/br/com/alura/forum/repository/TopicoRepositoryTest.java
@@ -1,20 +1,21 @@
 package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.modelo.Usuario;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
-
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class TopicoRepositoryTest {
 
     @Autowired
